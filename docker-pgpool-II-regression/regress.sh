7867e62a3e4cce6750e5f14f5b6aa6b9291323d8
Bo Peng <pengbo@sraoss.co.jp>
2022-06-05 19:08:16 +0900

    Add option -c.

--- docker-pgpool-II-regression/regress.sh
+++ docker-pgpool-II-regression/regress.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 TIMEOUT=600
-DEBUG=
+DEBUG=""
+TEST_SAMPLES=""
 
 if [ $PGPOOL_BRANCH = master -o `echo "$POOLVER >= 3.5" | bc` == 1 ];then
     DEBUG=-d
@@ -17,6 +18,7 @@ fi
 
 if [ $BUILD_RPM -eq 1 ]; then
     INSTALL_MODE=noinstall
+    TEST_SAMPLES=-c
     PGPOOL_PATH=/usr
 else
     INSTALL_MODE=install
@@ -26,5 +28,4 @@ fi
 cd /var/volum/$DIRNAME
 make clean
 cd /var/volum/${DIRNAME}/$TEST_DIR/regression
-./regress.sh -p /usr/pgsql-${PGSQL_VERSION}/bin -j /usr/share/${JDBC_DRIVER} -s ${PGSOCKET_DIR} -t ${TIMEOUT} -i ${PGPOOL_PATH} -m ${INSTALL_MODE}
-#./regress.sh -p /usr/pgsql-${PGSQL_VERSION}/bin -j /usr/share/$JDBC_DRIVER -s $PGSOCKET_DIR -t $TIMEOUT $DEBUG 001
+./regress.sh -p /usr/pgsql-${PGSQL_VERSION}/bin -j /usr/share/${JDBC_DRIVER} -s ${PGSOCKET_DIR} -t ${TIMEOUT} -i ${PGPOOL_PATH} -m ${INSTALL_MODE} ${TEST_SAMPLES}
